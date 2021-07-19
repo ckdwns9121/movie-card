f336b9a754f99c5abfb51107c790086a4f6979cf
ckdwns9121 <ckdwns9121@naver.com>
2021-07-19 14:05:00 +0900

    # create axios object

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -1,34 +1,16 @@
 import axios from 'axios';
 import { customAxiosInstance ,cancleToken} from './init';
-
-
-// export const requestGetMovieList = async(genre: string='')=>{
-//     try{
-
-//         const URL = `https://yts.mx/api/v2/list_movies.json?limit=50${genre === undefined ? '' : `&genre=${genre}`}`
-//         const res = axios.get(URL).then((res:any) =>{return res}).catch(()=>[]);
-//         return res;
-//     }
-//     catch(e){
-//         console.error(e);
-//     }
-// }
+const instance = customAxiosInstance();
 
 export const requsetGetMovie = async(id:string ,source : any) :Promise<any> =>{
-    try{
-        const URL = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
-        const res = await axios.get(URL,{cancelToken : source.token})
-        return res;
-    }
-    catch(e){
-        console.error(e);
-    }
+    const URL = `/api/v2/movie_details.json?movie_id=${id}}`
+    const res = await axios.get(URL,{cancelToken : source.token})
+    return res;
 }
 
 
 export const requestGetMovieList = async(genre: string=''):Promise<any>  =>{
     const URL = `/api/v2/list_movies.json?limit=50${genre === undefined ? '' : `&genre=${genre}`}`
-    const instance = customAxiosInstance();
     const res = await instance.get(URL);
     return res;
 }
