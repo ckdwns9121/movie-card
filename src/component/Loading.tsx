f943f288aa30bfc5cf3be69294f531ce1f34ae6e
ChangjunPark <ckdwns9121@naver.com>
2022-01-13 17:20:01 +0900

    # 로딩 수정

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -1,9 +1,9 @@
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
-import useLoading from '../hooks/loading';
+import useLoading from '../hooks/useLoading';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
     color: '#fff',
@@ -12,12 +12,10 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Loading() {
   const classes = useStyles();
-  const {loading} = useLoading();
+  const { loading } = useLoading();
   return (
-    <div>
-      <Backdrop className={classes.backdrop} open={loading}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    </div>
+    <Backdrop className={classes.backdrop} open={loading}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
   );
 }
