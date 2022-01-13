19fab426a74639c4b8edbe5d071cce02cd2e7ce7
ChangjunPark <ckdwns9121@naver.com>
2022-01-13 17:19:19 +0900

    # 무한 스크롤링 추가

--- src/container/MovieListContainer.tsx
+++ src/container/MovieListContainer.tsx
@@ -1,12 +1,34 @@
+import { useEffect } from 'react';
+import styles from './MovieContainer.module.scss';
 import MovieList from '../component/MovieList';
+import useScroll from '../hooks/useScroll';
+//hooks
+import { useDispatch, useSelector } from 'react-redux';
+import useLoading from '../hooks/useLoading';
+
+//store
 import { RootState } from '../store';
-import { useSelector } from 'react-redux';
-import BottomModal from '../component/BottomModal';
+import { getMovies } from '../store/movies';
 
 function MovieListContainer() {
-  const { movies } = useSelector((state: RootState) => state.movie);
+  const { movies, loading, page } = useSelector((state: RootState) => state.movie);
+  const dispatch = useDispatch();
 
-  return <>{movies.length !== 0 && <MovieList movies={movies} />}</>;
+  const { scrollEnd } = useScroll();
+  const { handleLoading } = useLoading();
+
+  useEffect(() => {
+    console.log(scrollEnd);
+    if (scrollEnd) {
+      dispatch(getMovies({ page }));
+    }
+  }, [scrollEnd]);
+
+  useEffect(() => {
+    handleLoading(loading);
+  }, [loading]);
+
+  return <div className={styles['container']}>{movies.length !== 0 && <MovieList movies={movies} />}</div>;
 }
 
 export default MovieListContainer;
