f2b0010ed8d62a772523fd793becb40423e1694f
ChangjunPark <ckdwns9121@naver.com>
2022-01-13 17:20:14 +0900

    # 가상 스크롤 연습

--- src/component/MovieList.tsx
+++ src/component/MovieList.tsx
@@ -1,47 +1,91 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import styles from './MovieList.module.scss';
-import {useHistory} from 'react-router-dom';
-import {ButtonBase} from '@material-ui/core';
-import {Movie} from '../types/Movie';
+import { useHistory } from 'react-router-dom';
+import { ButtonBase } from '@material-ui/core';
+import { WindowScroller, CellMeasurer, CellMeasurerCache, AutoSizer, List, ListRowProps } from 'react-virtualized';
 
-type MovieItemProps={
-    movie : Movie;
-}
-type MovieListProps={
-    movies : Movie[]
-}
+import { Movie } from '../types/Movie';
 
-function MovieItem ( {movie} : MovieItemProps){
+type MovieItemProps = {
+  movie: Movie;
+  onLoad?: any;
+};
+type MovieListProps = {
+  movies: Movie[];
+};
 
-    const history = useHistory();
-    const {id, medium_cover_image,title,rating,description_full} = movie;
+const cache = new CellMeasurerCache({
+  defaultWidth: 50,
+  fixedWidth: true,
+});
 
-    const onClickItem =()=>{
-        history.push(`/detail/${id}`);
-    }
-    return(
-        <ButtonBase className={styles['movie-item']} onClick={onClickItem}>
-            <div className={styles['movie-poster']}>
-                <img src={medium_cover_image} alt={title}/>
-            </div>
-            <div className={styles['movie-title']}>
-                {title}
-            </div>
-            <div className={styles['movie-description']}>
-                {description_full}
-            </div>
-        </ButtonBase>
-    )
-}
+function MovieItem({ movie, onLoad }: MovieItemProps) {
+  const history = useHistory();
 
-function MovieList({movies} : MovieListProps){
-    const list = movies.map(( movie : Movie)=> 
-    <MovieItem  key={movie.id} movie={movie} />)
-    return(
-        <div className={styles['movie-list']}>
-            {list}
+  const { id, medium_cover_image, title, rating, description_full } = movie;
+
+  const onClickItem = () => {
+    history.push(`/detail/${id}`);
+  };
+  return (
+    <ButtonBase className={styles['movie-item']} onClick={onClickItem}>
+      <div className={styles['movie-block']}>
+        <div className={styles['movie-poster']}>
+          <img src={medium_cover_image} alt={title} onLoad={onLoad} />
         </div>
-    )
+        <div className={styles['movie-title']}>{title}</div>
+        <div className={styles['movie-description']}>{description_full}</div>
+      </div>
+    </ButtonBase>
+  );
+}
+
+function MovieList({ movies }: MovieListProps) {
+  const list = movies.map((movie: Movie) => <MovieItem key={movie.id} movie={movie} />);
+  // const listRef = useRef<List | null>(null);
+
+  // const rowRenderer = ({ index, key, parent, style }: ListRowProps) => {
+  //   const movie = movies[index];
+  //   return (
+  //     <CellMeasurer cache={cache} parent={parent} key={key} columnIndex={0} rowIndex={index}>
+  //       {({ measure }) => (
+  //         <div style={style}>
+  //           <MovieItem movie={movie} onLoad={measure} />
+  //         </div>
+  //       )}
+  //     </CellMeasurer>
+  //   );
+  // };
+
+  // return (
+  //   <WindowScroller>
+  //     {({ height, scrollTop, isScrolling, onChildScroll }) => (
+  //       <AutoSizer disableHeight>
+  //         {({ width }) => {
+  //           console.log(width);
+  //           return (
+  //             <List
+  //               ref={listRef}
+  //               isScrolling={isScrolling}
+  //               onScroll={onChildScroll}
+  //               scrollTop={scrollTop}
+  //               autoHeight
+  //               width={width}
+  //               height={height}
+  //               rowCount={movies.length}
+  //               rowHeight={cache.rowHeight}
+  //               rowRenderer={rowRenderer}
+  //               list={movies}
+  //               deferredMeasurementCache={cache}
+  //               style={{ outline: 'none' }}
+  //             />
+  //           );
+  //         }}
+  //       </AutoSizer>
+  //     )}
+  //   </WindowScroller>
+  // );
+  return <div className={styles['movie-list']}>{list}</div>;
 }
 
 export default React.memo(MovieList);
