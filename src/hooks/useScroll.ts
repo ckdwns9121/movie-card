04131b0d8d3c0f7e0fc71729ee2e58900d1cc842
ChangjunPark <ckdwns9121@naver.com>
2022-01-26 16:01:53 +0900

    # 스크롤 쓰로들링 추가

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,29 +1,34 @@
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store';
+import { useState, useEffect, useCallback } from 'react';
+
+let timer: any = null;
 
 function useScroll() {
   const [scrollEnd, setScrollEnd] = useState<boolean>(false);
-  const { loading } = useSelector((state: RootState) => state.movie);
 
-  const handleScroll = () => {
+  const handleScroll = useCallback(() => {
     // 화면에 보이지 않은 높이도 포함된 페이지의 총 높이.
     const scrollHeight: number = document.documentElement.scrollHeight;
     // 이미 스크롤 되어서 보이지 않은 구간의 높이.
     const scrollTop: number = document.documentElement.scrollTop;
     // 사용자에게 보여지는 페이지의 높이.
     const clientHeight: number = document.documentElement.clientHeight;
-    setScrollEnd(scrollTop + clientHeight >= scrollHeight && !loading);
-  };
-
+    setScrollEnd(scrollTop + clientHeight >= scrollHeight);
+  }, []);
+  const throttle = useCallback(() => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      handleScroll();
+      timer = null;
+    }, 300);
+  }, [handleScroll]);
   useEffect(() => {
     // scroll event listener 등록
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', throttle);
     return () => {
       // scroll event listener 해제
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', throttle);
     };
-  });
+  }, [throttle]);
   return { scrollEnd, setScrollEnd, handleScroll };
 }
 export default useScroll;
