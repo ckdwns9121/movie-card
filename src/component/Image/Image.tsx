46d7cd249b186244555c6eefe0c2da5a9ab61af5
ChangjunPark <ckdwns9121@naver.com>
2022-06-29 10:46:41 +0900

    # image lazy loading

---
+++ src/component/Image/Image.tsx
@@ -0,0 +1,33 @@
+import { useRef, useState, useEffect } from "react";
+interface IProps {
+  src: string;
+  alt: string;
+}
+const PLACE_HOLDER = "https://uploads.codesandbox.io/uploads/user/6d7a2e27-f6e6-4ec3-a76a-fca4e0a3fb22/EF_m-placeholder.jpg";
+export default function Image({ src }: IProps) {
+  const imgRef = useRef<HTMLImageElement>(null);
+  const observerRef = useRef<IntersectionObserver>();
+  const [isLoad, setIsLoad] = useState(false);
+
+  function onIntersection(entries: IntersectionObserverEntry[], io: IntersectionObserver) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        io.unobserve(entry.target);
+        setIsLoad(true);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (!observerRef.current) {
+      observerRef.current = new IntersectionObserver(onIntersection, {
+        // 확인을 위해 이미지 절반이 나타날 때 로딩한다.
+        threshold: 0.5,
+      });
+    }
+
+    imgRef.current && observerRef.current.observe(imgRef.current);
+  }, []);
+
+  return <img ref={imgRef} src={isLoad ? src : PLACE_HOLDER} width="300" height="200px" alt="" style={{ display: "block", margin: "30px" }} />;
+}
